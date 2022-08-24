fac16bc7753fc2ec1d33c3beefdd2001f19f5e4d
VASILPC\vasil <vasilkosturski@gmail.com>
2022-08-24 09:35:54 +0300

    Project created

---
+++ src/MicroservicesCqrsKafka/OrdersService/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+using System.Threading.Tasks;
+
+namespace OrdersService;
+
+internal class Program
+{
+    public static async Task Main(string[] args)
+    {
+        Console.WriteLine("Hello");
+        await Task.Delay(0);
+    }
+}

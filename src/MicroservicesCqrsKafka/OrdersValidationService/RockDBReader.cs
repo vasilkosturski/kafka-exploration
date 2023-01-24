93f0ca3278a287b83e93308a48aec83e3c20e52b
VASILPC\vasil <vasilkosturski@gmail.com>
2023-01-24 08:54:20 +0200

    RocksDb reader improvements

--- src/MicroservicesCqrsKafka/OrdersValidationService/RockDBReader.cs
+++ src/MicroservicesCqrsKafka/OrdersValidationService/RockDBReader.cs
@@ -8,29 +8,29 @@ public class RockDBReader
 {
     public static void Read()
     {
-        ColumnFamilyOptions columnFamilyOptions = new ColumnFamilyOptions();
-        //this.writeOptions = new WriteOptions();
-        BlockBasedTableOptions table_options = new BlockBasedTableOptions();
-        //RocksDbOptions rocksDbOptions = new RocksDbOptions(dbOptions, columnFamilyOptions);
-        
-        DbOptions dbOptions = new DbOptions();
-
-        using (var db = RocksDb.Open(dbOptions, @"./test-app/1-0/rocksdb/orders-products-quantities", new ColumnFamilies(columnFamilyOptions)))
+        var dbOptions = new DbOptions();
+        using (var db = RocksDb.Open(dbOptions, @"./test-app/1-0/rocksdb/orders-products-quantities", 
+                   new ColumnFamilies(new ColumnFamilyOptions())))
         {
             // Using strings below, but can also use byte arrays for both keys and values
             //db.Put("key", "value");
             //string value = db.MultiGet(1)
+            
+            //db.NewIterator()
 
             var keyToInsert = 100;
             var valueToInsert = 200;
-            
-            var keyAsBytes = ToBytes(0);
-            var valueAsBytes = ToBytes(valueToInsert);
 
-            //db.Put(keyAsBytes, valueAsBytes);
+            var intSerDes = new Int32SerDes();
+
+            // var keyAsBytes = ToBytes(1);
+
+            var key = 1;
+            
+            var keyAsBytes = intSerDes.Serialize(key, new SerializationContext());
 
             var hasKey = db.HasKey(keyAsBytes);
-            
+
             var dbResponse = db.Get(keyAsBytes, db.GetDefaultColumnFamily());
             
             var serdes = new ValueAndTimestampSerDes<int>(new Int32SerDes());
@@ -41,21 +41,5 @@ public class RockDBReader
             //var res = FromBytes(dbResponse);
             //var res = System.Text.Encoding.UTF8.GetString(dbResponse);;
         }
-    }
-
-    private static byte[] ToBytes(int from)
-    {
-        var intBytes = BitConverter.GetBytes(from);
-        if (BitConverter.IsLittleEndian)
-            Array.Reverse(intBytes);
-        return intBytes;
-    }
-    
-    private static int FromBytes(byte[] from)
-    {
-        if (BitConverter.IsLittleEndian)
-            Array.Reverse(from);
-
-        return BitConverter.ToInt32(from, 0);
     }
 }

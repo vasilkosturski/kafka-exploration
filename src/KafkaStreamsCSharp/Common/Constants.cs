39fa28cfd8b653fd7ebf3377ee3b65b8a1a91504
VASILPC\vasil <vasilkosturski@gmail.com>
2023-02-08 08:37:49 +0200

    Small fixes

--- src/KafkaStreamsCSharp/Common/Constants.cs
+++ src/KafkaStreamsCSharp/Common/Constants.cs
@@ -1,6 +1,6 @@
 ﻿namespace Common;
 
-public class Constants
+public static class Constants
 {
     public const string ApplicationName = "test-app";
     public const string BootstrapServers = "localhost:9092,localhost:9093";

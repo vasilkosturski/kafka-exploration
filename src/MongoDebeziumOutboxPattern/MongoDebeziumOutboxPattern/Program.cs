4efdc46c05ca4c8e8ff84ff78ec6ff7d60b237b7
vasilkosturski <v.kosturski.ext@sportradar.com>
2023-06-13 08:54:24 +0300

    Adding Kafka consumer

--- src/MongoDebeziumOutboxPattern/MongoDebeziumOutboxPattern/Program.cs
+++ src/MongoDebeziumOutboxPattern/MongoDebeziumOutboxPattern/Program.cs
@@ -1,12 +1,34 @@
-﻿using MongoDB.Bson.Serialization.Conventions;
+﻿using Confluent.Kafka;
+using MongoDB.Bson.Serialization.Conventions;
 using MongoDebeziumOutboxPattern;
 
-MongoSetup();
-
+ConfigMongo();
 await new UserService().CreateUserAsync();
+ConsumeMessage();
 
-void MongoSetup()
+void ConfigMongo()
 {
     var conventionPack = new ConventionPack { new CamelCaseElementNameConvention() };
     ConventionRegistry.Register("camelCase", conventionPack, _ => true);
+}
+
+static void ConsumeMessage()
+{
+    var consumerConfig = new ConsumerConfig
+    {
+        BootstrapServers = "localhost:9092",
+        GroupId = "cgid",
+        AutoOffsetReset = AutoOffsetReset.Earliest
+    };
+    using var consumer = new ConsumerBuilder<string, string>(consumerConfig).Build();
+    consumer.Subscribe(new[] { "outbox.event.user" });
+
+    var consumeResult = consumer.Consume(TimeSpan.FromMilliseconds(10000));
+    var consumedMessage = consumeResult.Message;
+    var partition = consumeResult.Partition;
+    var key = consumeResult.Message.Key;
+    var offset = consumeResult.Offset;
+    //var dataJson = JsonSerializer.Serialize(data, SerializationOptions);
+
+    Console.WriteLine($"Partition: {partition} Key: {key} Offset: {offset} Data: {consumedMessage}");
 }

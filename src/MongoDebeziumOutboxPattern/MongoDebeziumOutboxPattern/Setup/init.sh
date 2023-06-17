0193afa952e3b484e5ae3e1201b8704f072569d1
vasilkosturski <v.kosturski.ext@sportradar.com>
2023-06-17 18:07:13 +0300

    Updated mongo version

--- src/MongoDebeziumOutboxPattern/MongoDebeziumOutboxPattern/Setup/init.sh
+++ src/MongoDebeziumOutboxPattern/MongoDebeziumOutboxPattern/Setup/init.sh
@@ -1,7 +1,7 @@
 docker-compose -f docker-compose-mongodb.yaml up -d
 
 # Check the status of MongoDB and wait until it's ready
-until docker run --network host --rm mongo:5.0 mongo --host localhost --eval "db.adminCommand('ping')"
+until docker run --network host --rm mongo:6.0 mongosh --host localhost --eval "db.adminCommand('ping')"
 do
     echo "Waiting for MongoDB..."
     sleep 5s
@@ -9,11 +9,11 @@ done
 
 # Initiate the replica set
 echo "Initiating the MongoDB replica set"
-docker run --network host --rm mongo:5.0 mongo --host localhost --eval 'rs.initiate({_id: "rs0", members: [{_id: 0, host: "localhost:27017"}]})'
+docker run --network host --rm mongo:6.0 mongosh --host localhost --eval 'rs.initiate({_id: "rs0", members: [{_id: 0, host: "localhost:27017"}]})'
 echo "MongoDB replica set initiated"
 
 # Wait for the replica set to be fully initialized
-until docker run --network host --rm mongo:5.0 mongo --host localhost --eval 'rs.status().myState == 1'
+until docker run --network host --rm mongo:6.0 mongosh --host localhost --eval 'rs.status().myState == 1'
 do
     echo "Waiting for MongoDB replica set to fully initialize..."
     sleep 3s
@@ -21,7 +21,7 @@ done
 echo "MongoDB replica set is fully initialized."
 
 echo "Creating 'testdb' database and 'outbox' collection..."
-docker run --network host --rm mongo:5.0 mongo --host localhost --eval '
+docker run --network host --rm mongo:6.0 mongosh --host localhost --eval '
     db = db.getSiblingDB("testdb");
     db.createCollection("outbox");
 '
